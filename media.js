f7072d81f95f41f82d5f2156ff23e1b5566e6a6b
Britseidy <britseidyaj@gmail.com>
2023-06-27 15:43:59 -0600

    Finalización del dia

---
+++ media.js
@@ -0,0 +1,15 @@
+let btn = document.querySelectorAll(".opcion-btn");
+let popup = document.querySelector(".popup");
+let nJuegobtn = document.getElementById("#Nuevo-juego");
+let reanudarbtn = document.getElementById("#reanudar");
+let mensbtn = document.getElementById("#mensaje");
+let ganador = [
+  [0, 1, 2],
+  [0, 3, 6],
+  [2, 5, 8],
+  [6, 7, 8],
+  [3, 4, 5],
+  [1, 4, 7],
+  [0, , 4, 8],
+  [2, 4, 6],
+];
